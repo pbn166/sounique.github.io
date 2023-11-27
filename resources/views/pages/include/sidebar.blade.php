2fc9c6aa3f81ad6aafe0139f2a2cf60ad26aed70
pahn.anhi <nhib1906335@student.ctu.edu.vn>
2023-11-27 19:50:05 +0700

    First
    Fix:
    
    - Xoá sản phẩm yêu thích
    - Thêm giỏ hàng (button)
    - Page yêu thích
    - Liên hệ
    - Show_checkout
    - Admin

---
+++ resources/views/pages/include/sidebar.blade.php
@@ -0,0 +1,21 @@
+<div class="col-sm-3">
+  <div class="left-sidebar">
+    <div class="brands_products">
+      <!--brands_products-->
+      <h2>Sản phẩm yêu thích</h2>
+      <div class="brands-name">
+        <div id="row_wishlist" class="row"></div>
+      </div>
+    </div>
+    <!--/brands_products-->
+
+    <div class="brands_products">
+      <!--brands_products-->
+      <h2>Sản phẩm đã xem</h2>
+      <div class="brands-name">
+        <div id="row_viewed" class="row"></div>
+      </div>
+    </div>
+    <!--/brands_products-->
+  </div>
+</div>
